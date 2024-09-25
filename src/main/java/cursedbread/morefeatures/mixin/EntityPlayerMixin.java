d77091fba5d029bcdcbaf9056da48a1d308feda9
average-bread <163648686+average-bread@users.noreply.github.com>
2024-09-25 09:54:08 +0200

    tried to fix multiplayer problem

--- src/main/java/cursedbread/morefeatures/mixin/EntityPlayerMixin.java
+++ src/main/java/cursedbread/morefeatures/mixin/EntityPlayerMixin.java
@@ -15,27 +15,28 @@ public class EntityPlayerMixin {
 	@Shadow
 	protected float baseSpeed;
 
+	public boolean isClientSide;
+
 	@Inject(method = "onLivingUpdate()V", at = @At("TAIL"))
 	private void armor_effects(CallbackInfo ci) {
-		if (FeaturesItems.olivineArmorEnabled == 1){
+		if (FeaturesItems.olivineArmorEnabled == 1 && !isClientSide){
+			this.isClientSide = false;
 			ItemStack helmet_item = Minecraft.getMinecraft(this).thePlayer.inventory.armorItemInSlot(3);
 			ItemStack chest_item = Minecraft.getMinecraft(this).thePlayer.inventory.armorItemInSlot(2);
 			ItemStack leggings_item = Minecraft.getMinecraft(this).thePlayer.inventory.armorItemInSlot(1);
 			ItemStack boots_item = Minecraft.getMinecraft(this).thePlayer.inventory.armorItemInSlot(0);
-			if (FeaturesItems.olivineArmorEnabled == 1){
-				if (chest_item != null && chest_item.getItem().equals(FeaturesItems.olivineChestplate)) {
-					this.baseSpeed = 0.2f;
-				} else if (leggings_item != null && leggings_item.getItem().equals(FeaturesItems.olivineLeggings)) {
-					this.baseSpeed = 0.17f;
-				} else if (helmet_item != null && FeaturesItems.regularCrownEnabled == 1 && helmet_item.getItem().equals(FeaturesItems.olivineCrown)) {
-					this.baseSpeed = 0.15f;
-				} else if (helmet_item != null && helmet_item.getItem().equals(FeaturesItems.olivineHelmet)) {
-					this.baseSpeed = 0.15f;
-				} else  if (boots_item != null && boots_item.getItem().equals(FeaturesItems.olivineBoots)) {
-					this.baseSpeed = 0.13f;
-				} else {
-					this.baseSpeed = 0.1f;
-				}
+			if (chest_item != null && chest_item.getItem().equals(FeaturesItems.olivineChestplate)) {
+				this.baseSpeed = 0.2f;
+			} else if (leggings_item != null && leggings_item.getItem().equals(FeaturesItems.olivineLeggings)) {
+				this.baseSpeed = 0.17f;
+			} else if (helmet_item != null && FeaturesItems.regularCrownEnabled == 1 && helmet_item.getItem().equals(FeaturesItems.olivineCrown)) {
+				this.baseSpeed = 0.15f;
+			} else if (helmet_item != null && helmet_item.getItem().equals(FeaturesItems.olivineHelmet)) {
+				this.baseSpeed = 0.15f;
+			} else  if (boots_item != null && boots_item.getItem().equals(FeaturesItems.olivineBoots)) {
+				this.baseSpeed = 0.13f;
+			} else {
+				this.baseSpeed = 0.1f;
 			}
 		}
 	}
